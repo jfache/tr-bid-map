9ea335a0d7051bae0f1c111d8138fd0b3d70c7f1
Tushar Ghate <tushghate@gmail.com>
2018-05-04 16:09:11 -0400

    Added gradient to activity feed

--- src/components/ActivityFeed.vue
+++ src/components/ActivityFeed.vue
@@ -8,6 +8,7 @@
                 <activity v-bind="activity"/>
             </div>
         </div>
+        <div class="activity-feed__gradient"></div>
     </div>
 </template>
 
@@ -36,13 +37,33 @@ export default {
     top: 0;
     left: 5%;
     height: 80vh;
-    width: 40vh;
+    width: 35vh;
     margin-top: 5vh;
     padding: 40px;
     background: #fff;
     box-shadow: 0px 15px 30px 0 rgba(0, 0, 0, 0.25);
     border-radius: 20px;
     overflow: hidden;
+}
+
+.activity-feed__gradient {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 30vh;
+    background: -moz-linear-gradient(top, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 100%); /* FF3.6-15 */
+    background: -webkit-linear-gradient(
+        top,
+        rgba(255, 255, 255, 0) 0%,
+        rgba(255, 255, 255, 1) 100%
+    ); /* Chrome10-25,Safari5.1-6 */
+    background: linear-gradient(
+        to bottom,
+        rgba(255, 255, 255, 0) 0%,
+        rgba(255, 255, 255, 1) 100%
+    ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffff', endColorstr='#ffffff',GradientType=0 ); /* IE6-9 */
 }
 
 .activity-feed__title {
