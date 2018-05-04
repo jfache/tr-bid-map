53e560e331051408a54c1d4b8a5c6632578d239a
Jeremy Fache <jeremy.fache@traderev.com>
2018-05-04 09:59:32 -0400

    add map

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <mapbox
+        access-token="pk.eyJ1IjoibHV1a3ZhbmJhYXJzIiwiYSI6ImNqZ3Jia3pyMjAwa3Myd2xlczhzYWk3NWsifQ.VNQ_VAyPIF2BaZEo4lztFw"
+        :map-options="mapOptions"></mapbox>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Mapbox from 'mapbox-gl-vue';
+
+const defaultOptions = {
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-79.383184, 43.653226],
+    zoom: 15
+}
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        Mapbox
+    },
+    data: function() {
+        return {
+            mapOptions: defaultOptions
+        }
+    }
 }
 </script>
 
 <style>
+body {
+    margin: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+#map {
+    width: 100%;
+    height: 90vh;
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+let webpack = require('webpack');
+
+module.exports = {
+    configureWebpack: {
+        plugins: [
+            new webpack.ProvidePlugin({
+                mapboxgl: 'mapbox-gl'
+            })
+        ]
+    }
+}
