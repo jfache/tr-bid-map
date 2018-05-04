bcff62e2dbe8cf936202243b5e911957c51777fe
Tushar Ghate <tushghate@gmail.com>
2018-05-04 10:46:51 -0400

    Fix formatting in App.vue

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ const defaultOptions = {
     style: 'mapbox://styles/mapbox/streets-v9',
     center: [-79.383184, 43.653226],
     zoom: 15
-}
+};
 
 export default {
     name: 'app',
@@ -24,60 +24,65 @@ export default {
     data: function() {
         return {
             mapOptions: defaultOptions
-        }
+        };
     },
     methods: {
         mapClicked(map) {
             map.addLayer({
-        'id': 'points',
-        'type': 'symbol',
-        'source': {
-          'type': 'geojson',
-          'data': {
-            'type': 'FeatureCollection',
-            'features': [{
-              'type': 'Feature',
-              'geometry': {
-                'type': 'Point',
-                'coordinates': [-77.03238901390978, 38.913188059745586]
-              },
-              'properties': {
-                'title': 'Mapbox DC',
-                'icon': 'monument'
-              }
-            }, {
-              'type': 'Feature',
-              'geometry': {
-                'type': 'Point',
-                'coordinates': [-122.414, 37.776]
-              },
-              'properties': {
-                'title': 'Mapbox SF',
-                'icon': 'harbor'
-              }
-          }, {
-              type: 'Feature',
-              geometry: {
-                  type: 'Point',
-                  coordinates: defaultOptions.center
-              },
-              properties: {
-                  title: 'Test Toronto',
-                  icon: 'account'
-              }
-          }]}
-        },
-        'layout': {
-          'icon-image': '{icon}-15',
-          'text-field': '{title}',
-          'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-          'text-offset': [0, 0.6],
-          'text-anchor': 'top'
-        }
-      });
+                id: 'points',
+                type: 'symbol',
+                source: {
+                    type: 'geojson',
+                    data: {
+                        type: 'FeatureCollection',
+                        features: [
+                            {
+                                type: 'Feature',
+                                geometry: {
+                                    type: 'Point',
+                                    coordinates: [-77.03238901390978, 38.913188059745586]
+                                },
+                                properties: {
+                                    title: 'Mapbox DC',
+                                    icon: 'monument'
+                                }
+                            },
+                            {
+                                type: 'Feature',
+                                geometry: {
+                                    type: 'Point',
+                                    coordinates: [-122.414, 37.776]
+                                },
+                                properties: {
+                                    title: 'Mapbox SF',
+                                    icon: 'harbor'
+                                }
+                            },
+                            {
+                                type: 'Feature',
+                                geometry: {
+                                    type: 'Point',
+                                    coordinates: defaultOptions.center
+                                },
+                                properties: {
+                                    title: 'Test Toronto',
+                                    icon: 'account'
+                                }
+                            }
+                        ]
+                    }
+                },
+                layout: {
+                    'icon-image': '{icon}-15',
+                    'text-field': '{title}',
+                    'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+                    'text-offset': [0, 0.6],
+                    'text-anchor': 'top'
+                }
+            });
         }
     }
-}
+};
 </script>
 
 <style>
