fedd170037bcb9e78342f03231d4daefe2cf296f
Jeremy Fache <jeremy.fache@traderev.com>
2018-05-04 10:35:12 -0400

    test methods

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <mapbox
         access-token="pk.eyJ1IjoibHV1a3ZhbmJhYXJzIiwiYSI6ImNqZ3Jia3pyMjAwa3Myd2xlczhzYWk3NWsifQ.VNQ_VAyPIF2BaZEo4lztFw"
-        :map-options="mapOptions"></mapbox>
+        :map-options="mapOptions"
+        @map-click="mapClicked"></mapbox>
   </div>
 </template>
 
@@ -23,6 +24,57 @@ export default {
     data: function() {
         return {
             mapOptions: defaultOptions
+        }
+    },
+    methods: {
+        mapClicked(map) {
+            map.addLayer({
+        'id': 'points',
+        'type': 'symbol',
+        'source': {
+          'type': 'geojson',
+          'data': {
+            'type': 'FeatureCollection',
+            'features': [{
+              'type': 'Feature',
+              'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.03238901390978, 38.913188059745586]
+              },
+              'properties': {
+                'title': 'Mapbox DC',
+                'icon': 'monument'
+              }
+            }, {
+              'type': 'Feature',
+              'geometry': {
+                'type': 'Point',
+                'coordinates': [-122.414, 37.776]
+              },
+              'properties': {
+                'title': 'Mapbox SF',
+                'icon': 'harbor'
+              }
+          }, {
+              type: 'Feature',
+              geometry: {
+                  type: 'Point',
+                  coordinates: defaultOptions.center
+              },
+              properties: {
+                  title: 'Test Toronto',
+                  icon: 'account'
+              }
+          }]}
+        },
+        'layout': {
+          'icon-image': '{icon}-15',
+          'text-field': '{title}',
+          'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+          'text-offset': [0, 0.6],
+          'text-anchor': 'top'
+        }
+      });
         }
     }
 }
